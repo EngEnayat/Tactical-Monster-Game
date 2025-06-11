e81905ffa44443b71856d96dd1d4e0119b673d73
Eng.Enayat <enayatullahbalaghi2@gmail.com>
2025-06-11 09:55:35 +0330

    Remove build directory from repo

--- build/Desktop_Qt_6_8_2-Debug/CMakeFiles/PointerBoard.dir/PointerBoard_autogen/EWIEGA46WW/qrc_resources.cpp.o.d
+++
@@ -1,3 +0,0 @@
-CMakeFiles/PointerBoard.dir/PointerBoard_autogen/EWIEGA46WW/qrc_resources.cpp.o: \
- /home/enayatullah-balaghi/Downloads/QT/QtExcercise/PointerBoard/build/Desktop_Qt_6_8_2-Debug/PointerBoard_autogen/EWIEGA46WW/qrc_resources.cpp \
- /usr/include/stdc-predef.h
--- build/Desktop_Qt_6_8_2-Debug/CMakeFiles/PointerBoard.dir/agents.cpp.o.d
+++
@@ -1,3 +0,0 @@
-CMakeFiles/PointerBoard.dir/agents.cpp.o: \
- /home/enayatullah-balaghi/Downloads/QT/QtExcercise/PointerBoard/agents.cpp \
- /usr/include/stdc-predef.h
